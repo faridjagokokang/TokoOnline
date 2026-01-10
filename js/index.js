145d348bb230c8fb93c3d4e3e95950b16aaf721a
Muh. Farid Donovant <ridd2all.done@gmail.com>
2026-01-10 14:12:33 +0700

    hyg

--- js/index.js
+++ js/index.js
@@ -109,6 +109,10 @@ function loadDummyExclusiveTodayProducts() {
   const products = [
     { id:1, img:'gambar/crispy-hemat.jpg', name:'Paket Crispy Hemat', price:25000, Desc:'1 pcs ayam + nasi' },
     { id:2, img:'gambar/keluarga-cuan.jpg', name:'Paket Keluarga', price:75000, Desc:'4 pcs ayam + nasi' },
+    { id:3, img:'gambar/lezat.jpg', name:'Paket Lezat', price:20000, Desc:'1 pcs ayam + nasi + Red Velvet Coffee 1 + Yakult Mangga 1' },
+    { id:4, img:'gambar/Segar-gigit.jpg', name:'Paket Segar Gigit', price:40000, Desc:'1 pcs burger + 1 Green Tea Lemon' },
+    { id:5, img:'gambar/paket-frozeen-food.jpg', name:'Paket Frozen Food', price:60000, Desc:'1 pack ikan asin lendra + 1 pack ikan asin teri + 1 pack ikan asin keranjang + 1 pack sayap ayam potong' },
+  
   ];
   renderCardList('productToday', products);
 }
@@ -144,16 +148,175 @@ function loadProductCategories() {
 
 function loadDummyProducts() {
   const products = [
-    { id: 1, img: 'gambar/ayam-penuts-sauce.jpg', name: 'Ayam Penuts Sauce', price: 15000, Desc: 'Ayam dengan saus kacang', category: 'makanan' },
-    { id: 2, img: 'gambar/ayam-bakar.jpg', name: 'Ayam Bakar', price: 80000, Desc: 'Ayam bakar dengan bumbu khas.', category: 'makanan' },
-    { id: 3, img: 'gambar/ayam-katsu.jpg', name: 'Ayam Katsu', price: 25000, Desc: 'Ayam goreng tepung ala Jepang.', category: 'makanan' },
-    { id: 4, img: 'gambar/Burger.jpg', name: 'Burger', price: 30000, Desc: 'Burger dengan daging sapi dan sayuran segar.', category: 'makanan' },
-    { id: 5, img: 'gambar/kentang-goreng.jpg', name: 'Kentang Goreng', price: 15000, Desc: 'Kentang goreng renyah dan gurih.', category: 'makanan' },
-    { id: 6, img: 'gambar/Ammericano.jpg', name: 'Americano', price: 20000, Desc: 'Kopi Americano dengan rasa khas.', category: 'minuman' },
-    { id: 7, img: 'gambar/Cappucino.jpg', name: 'Cappucino', price: 25000, Desc: 'Cappucino dengan busa susu lembut.', category: 'minuman' },
-    { id: 8, img: 'gambar/Caramel-coffee-milk.jpg', name: 'Caramel Coffee Milk', price: 28000, Desc: 'Kopi karamel dengan susu lembut.', category: 'minuman' },
-    { id: 9, img: 'gambar/Sushi-garing-keranjang.jpg', name: 'Ikan Asin Keranjangan', price: 10000, Desc: 'Asin, Gurih, dan Maknyus', category: 'Frozen Food' },
-    { id: 10, img: 'gambar/Sushi-garing-lendra.jpg', name: 'Ikan Asin Lendra', price: 20000, Desc: 'Rasa mantap, Gurih', category: 'Frozen Food' },
+    { 
+      id: 1,
+      img: 'gambar/ayam-penuts-sauce.jpg',
+      name: 'Ayam Penuts Sauce',
+      price: 15000,
+      Desc: 'Ayam dengan saus kacang',
+      category: 'makanan'
+    },
+    { 
+      id: 2,
+      img: 'gambar/ayam-bakar.jpg',
+      name: 'Ayam Bakar',
+      price: 80000,
+      Desc: 'Ayam bakar dengan bumbu khas.',
+      category: 'makanan'
+    },
+    { 
+      id: 3,
+      img: 'gambar/ayam-katsu.jpg',
+      name: 'Ayam Katsu',
+      price: 25000,
+      Desc: 'Ayam goreng tepung ala Jepang.',
+      category: 'makanan'
+    },
+    { 
+      id: 4,
+      img: 'gambar/Burger.jpg',
+      name: 'Burger',
+      price: 30000,
+      Desc: 'Burger dengan daging sapi dan sayuran segar.',
+      category: 'makanan'
+    },
+    { 
+      id: 5,
+      img: 'gambar/kentang-goreng.jpg',
+      name: 'Kentang Goreng',
+      price: 15000,
+      Desc: 'Kentang goreng renyah dan gurih.',
+      category: 'makanan'
+    },
+    { 
+      id: 6,
+      img: 'gambar/Ammericano.jpg',
+      name: 'Americano',
+      price: 20000,
+      Desc: 'Kopi Americano dengan rasa khas.',
+      category: 'minuman'
+    },
+    { 
+      id: 7,
+      img: 'gambar/Cappucino.jpg',
+      name: 'Cappucino',
+      price: 25000,
+      Desc: 'Cappucino dengan busa susu lembut.',
+      category: 'minuman'
+    },
+    { 
+      id: 8,
+      img: 'gambar/Caramel-coffee-milk.jpg',
+      name: 'Caramel Coffee Milk',
+      price: 28000,
+      Desc: 'Kopi karamel dengan susu yang lembut.',
+      category: 'minuman'
+    },
+    {
+      id: 9,
+      img: 'gambar/Sushi-garing-keranjang.jpg',
+      name: 'Ikan Asin Keranjangan',
+      price: 10000,
+      Desc: 'Asin, Gurih, dan Maknyus',
+      category: 'Frozen Food'
+    },
+    {
+      id: 10,
+      img: 'gambar/Sushi-garing-lendra.jpg',
+      name: 'Ikan Asin lendra',
+      price: 20000,
+      Desc: 'rasanya mantap, Gurih, dan semlehot',
+      category: 'Frozen Food'
+    },
+    {
+      id: 11,
+      img: 'gambar/Sushi-garing-teri.jpg',
+      name: 'Ikan Asin Teri',
+      price: 20000,
+      Desc: 'rasanya mantap, Gurih, dan semlehot',
+      category: 'Frozen Food'
+    },
+    {
+      id: 12,
+      img: 'gambar/Sushi-garing-peda.jpg',
+      name: 'Ikan Asin peda',
+      price: 30000,
+      Desc: 'rasanya mantap, Gurih, dan semlehot',
+      category: 'Frozen Food'
+    },
+     {
+      id: 13,
+      img: 'gambar/teh-tarik-bakar.jpg',
+      name: 'teh tarik bakar',
+      price: 15000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 14,
+      img: 'gambar/teh-tarik.jpg',
+      name: 'teh tarik',
+      price: 5000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 15,
+      img: 'gambar/Vietnam-drip.jpg',
+      name: 'Vietnam Drip',
+      price: 20000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 16,
+      img: 'gambar/Yakult-light-1-pack.jpg',
+      name: 'Yakult Light 1 Pack',
+      price: 12000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 17,
+      img: 'gambar/Yakult-mangga-1-pak.jpg',
+      name: 'Yakult Mangga 1 Pack',
+      price: 12000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 18,
+      img: 'gambar/Yakult-original-1-pack.jpg',
+      name: 'Yakult Original 1 Pack',
+      price: 12000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 19,
+      img: 'gambar/green-tea-with-lemon.jpg',
+      name: 'Green Tea With Lemon',
+      price: 10000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 20,
+      img: 'gambar/lemon-tea.jpg',
+      name: 'Lemon Tea',
+      price: 10000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+    {
+      id: 21,
+      img: 'gambar/lychee-tea.jpg',
+      name: 'Lychee Tea',
+      price: 15000,
+      Desc: 'rasanya mantap, seger, dan cihuy',
+      category: 'minuman'
+    },
+
   ];
   allProducts = products;
   renderCardList('productGrid', products);
